36d3620e38a01003012fc2741754479bba9fb9ac
Bojidar Shopov <b_shopov@mail.bg>
2021-12-13 00:28:35 +0200

    Create a project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+
+
+
+<form method="post" enctype="multipart/form-data">
+    @csrf
+    <input type="file" name="get_file" value="" />
+    <input type="submit" name="submit" value="Calc" />
+</form>
